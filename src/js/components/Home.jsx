090331e3195638f5c7d178848fa7144653dd7ccc
lmatiasbelderrain <Matibelde@hotmail.com>
2025-02-17 18:27:39 +0000

    tictactoe complete

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+	const [board, setBoard] = useState(Array(9).fill(null))
+	const [xIsNext, setxIsNext] = useState(true)
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	const handleClick = (index) => {
+		if (board[index] || calculateWinner(board)) return
+		const newBoard = [...board]
+		newBoard[index]=xIsNext?"X":"O"
+		setBoard(newBoard)
+		setxIsNext(!xIsNext)
+	}
+
+	const calculateWinner = (squares) => {
+		const lines = [
+			[0, 1, 2], [3, 4, 5], [6, 7, 8],
+			[0, 3, 6], [1, 4, 7], [2, 5, 8],
+			[0, 4, 8], [2, 4, 6]
+		];
+		for (let line of lines) {
+			const [a, b, c] = line
+			if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+				return squares[a]
+			}
+		}
+		return null
+	}
+
+	const winner = calculateWinner(board)
+	const status = winner ? `Ganó ${winner}!` : `Turno de ${xIsNext ? "X" : "O"}`;
+
+	return (
+		<div className="container mt-5 text-center" >
+			<h2>
+				{status}
+			</h2>
+			<div style={{
+				display: "grid",
+				gridTemplateColumns: "repeat(3,80px)",
+				gap: "5px",
+				justifyContent: "center",
+			}}>
+				{board.map((cell, index) => (
+					<button
+						key={index}
+						onClick={() => handleClick(index)}
+						style={{
+							width: "80px",
+							height: "80px",
+							fontSize: "24px",
+							cursor: "pointer",
+							borderRadius: "5px",
+						}}
+					>
+						{cell}
+					</button>
+				))}
+			</div>
+			<div className="mt-2">
+				<button className="btn btn-outline-danger"
+				onClick={()=>setBoard(Array(9).fill(null))}
+				>Volver a comenzar</button>
+			</div>
+
 		</div>
 	);
 };
